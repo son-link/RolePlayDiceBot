4910920407ad8f8c1f76099e0fdc766cf1b69884
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2022-10-08 16:05:56 +0200

    Movidas las funciones de tiradas a un nueva clase

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const { I18n } = require('i18n');
 const path = require('path');
 const fse = require('fs-extra');
+const Dices = require('./lib/dices');
+const dices = new Dices();
 
 const i18n = new I18n({
   locales: ['en', 'es'],
@@ -10,7 +12,6 @@ const i18n = new I18n({
   defaultLocale: 'en',
 });
 
-//const __ = i18n.__;
 const confFile = './config/default.json'
 
 var config = require('./config/default.json');
@@ -21,88 +22,30 @@ const token = process.env.DICEBOT_TOKEN;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-const dice_standard = /\/([\d]+)d([\d]+)(\+[\d]+)?/;
-const dice_ryf = /\/ryf(6?)(\+|\-?)/;
-
-/**
- * Lanza un dado de las caras indicadas y devuelve un número aleatorio.
- * @param {int} caras El número de caras del dado
- * @returns {int} El resultado de la tirada.
- */
-function tirarDado(caras) {
-  return Math.floor(Math.random() * caras)+1;
-}
+const regex_standard = /\/([\d]+)d([\d]+)(\+[\d]+)?/;
+const regex_ryf = /\/ryf(6?)(\+|\-?)/;
 
 /**
  * Esta función es para cuando se hace una tira estándar de dados (X dados de Y caras)
  */
-bot.onText(dice_standard, (msg) => {
-  const dices = msg.text.match(dice_standard);
-  const tiradas = parseInt(dices[1]);
-  const caras = parseInt(dices[2]);
-  const opciones = msg.text.match(dice_standard);
-  let result = `${msg.from.first_name}: `;
-  let sumTiradas = 0;
-  let dados = '';
+bot.onText(regex_standard, (msg) => {
+  const result = dices.dice_standard(msg.text)
+  let text = __(
+    msg.chat.id,
+    `${msg.from.first_name}: ha tirado ${result.tiradas} dado(s) de ${result.caras} caras (${result.dados})`
+  );
 
-  if (typeof opciones[3] === 'undefined') {
-    for (i=0; i < tiradas; i++) {
-      result += tirarDado(caras) + ' ';
-    }
-  } else {
-    for (i=0; i < tiradas; i++) {
-      let tirada = tirarDado(caras);
-      sumTiradas += tirada;
-      dados += tirada + ' ';
-    }
-
-    let aSumar = opciones[3].replace('+', '');
-    sumTiradas += parseInt(aSumar);
-    result = __(`${msg.from.first_name}: ha tirado ${tiradas} dado(s) de ${caras} caras (${dados}) y se le ha sumado ${aSumar} al resultado = ${sumTiradas}`);
-  }
-  bot.sendMessage(msg.chat.id, result);
+  if (result.aSumar) text += ` y se le ha sumado ${result.aSumar} al resultado = ${result.sumTiradas}`;
+  if (result.aRestar) text += ` y se le ha restado ${result.aRestar} al resultado = ${result.sumTiradas}`;
+  bot.sendMessage(msg.chat.id, text);
 });
 
 /**
  * Esta es cuando se van a lanzar dados para el sistema de Rápido y Fácil (3 dados de 10 y se coge el valor medio)
  */
-bot.onText(dice_ryf, (msg) => {
-  let tiradas = [];
-  const opciones = msg.text.match(dice_ryf)
-  let caras = 10;
-  let resultado = null;
-  if (opciones[1] == 6) caras = 6
-  let result = `${msg.from.first_name}: `;
-
-  for (i=0; i < 3; i++) {
-    tiradas.push(tirarDado(caras));
-  }
-
-  if (!opciones[2]) {
-    // Si no se le esta pasando como opcional un + o - se obtiene el dado medio.
-    resultado = tiradas[0]+tiradas[1]+tiradas[2] - Math.min(tiradas[0], tiradas[1], tiradas[2]) - Math.max(tiradas[0], tiradas[1], tiradas[2]);
-  } else if (opciones[2] == '+') {
-    // En este caso se devuelve el dado mayor
-    resultado = Math.max(...tiradas);
-  } else if (opciones[2] == '-') {
-    // Y en este el menor
-    resultado = Math.min(...tiradas);
-  }
-  
-  bot.sendMessage(msg.chat.id, `${msg.from.first_name}: ${tiradas[0]} ${tiradas[1]} ${tiradas[2]} => ${resultado}`);
-});
-
-/**
- * Lanza 1 dado de 100 caras, también llamada porcentual
- */
-bot.onText(/\/d100/, (msg) => {
-  let lang = getChatLang(msg.chat.id);
-  const resultado = tirarDado(100);
-  const text = __(msg.chat.id, '{{ name }} roll 1d100: {{ result }}', {
-    name: msg.from.first_name,
-    result: resultado,
-  });
-
+bot.onText(regex_ryf, (msg) => {
+  const result = dices.dice_ryf(msg.text);
+  const text = `${msg.from.first_name} : ${result.dado1} ${result.dado2} ${result.dado3} => ${result.resultado}`
   bot.sendMessage(msg.chat.id, text);
 });
 
@@ -110,24 +53,9 @@ bot.onText(/\/d100/, (msg) => {
  * Lanza una tirada para el sistema FATE (4 dados de 6 caras, pero con símbolos +, - o nada)
  */
 bot.onText(/\/fate/, (msg) => {
-  let tiradas = [];
-  let resultado = 0;
-
-  for (i=0; i<4; i++) {
-    let tirada = tirarDado(6);
-    if (tirada == 1 || tirada == 2) {
-      tiradas.push('-');
-      resultado -= 1;
-    } else if (tirada == 3 || tirada == 4) {
-      tiradas.push('0');
-      resultado += 0;
-    } else {
-      tiradas.push('+');
-      resultado += 1;
-    }
-  }
-  let tirada = tiradas.join(', ');
-  bot.sendMessage(msg.chat.id, `${msg.from.first_name} saco ${tirada} : ${resultado}`);
+  const result = dices.fate();
+  const text = __(msg.chat.id, `${msg.from.first_name} saco ${result.tirada} : ${result.resultado}`);
+  bot.sendMessage(msg.chat.id, text);
 });
 
 /**
@@ -148,7 +76,7 @@ bot.on('polling_error', (error) => {
 
   *Comandos disponibles*
     
-    \\- Tirada clásica: /XdY \\(por ejemplo: /1d20, /2d6, /2d10, etc\\)\\. Es posible añadir una suma a la tirada añadiendo \\+ y el valor a sumar, por ejemplo /1d20\\+5
+    \\- Tirada clásica: /XdY \\(por ejemplo: /1d20, /2d6, /2d10, etc\\)\\. Es posible añadir una suma o resta a la tirada añadiendo \\+ ó \\- y el valor a sumar o restar, por ejemplo /1d20\\+5
     \\- /ryf: Tirada para el sistema Rápido y Fácil \\(tira 3 dados de 10 caras y coge el dado medio\\)\\. Este comando tiene variaciones:
       \\- /rfy\\+: En este caso se obtiene el dado alto
       \\- /ryf\\-: En este caso el dado menor
@@ -160,7 +88,6 @@ bot.on('polling_error', (error) => {
 });
 
 bot.onText(/\/config/, (msg) => {
-  console.log(msg);
   const opts = {
     reply_markup: {
       inline_keyboard: [
@@ -176,7 +103,6 @@ bot.onText(/\/config/, (msg) => {
   bot.sendMessage(msg.chat.id, __(msg.chat.id, 'Configure bot'), opts);
 });
 
-
 // Handle callback queries
 bot.on('callback_query', (callbackQuery) => {
   const action = callbackQuery.data;
@@ -188,7 +114,6 @@ bot.on('callback_query', (callbackQuery) => {
   };
   let text;
   
-  console.log(action);
   if (action === 'edit') {
     text = 'Edited Text';
   } else if (action == 'set_lang') {
